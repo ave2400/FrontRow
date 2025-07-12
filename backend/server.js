f0640b68db1f570a76ddea11e180fced5d1265fb
Aarushi <aaru.lakhi@gmail.com>
2025-07-12 16:10:36 -0500

    add DELETE request to allowed methods in CORS config to fix issue with deleting a stream on admin page

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
 
